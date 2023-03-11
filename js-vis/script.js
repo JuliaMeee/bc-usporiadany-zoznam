ebd3ac02178fdb166c4dc07eb080bbea083db3e9
Julia Michalkova <xmichalk@fi.muni.cz>
2023-03-11 11:38:02 +0100

    feat: python-impl, js-vis base

---
+++ js-vis/script.js
@@ -0,0 +1,67 @@
+let options = {
+    name: 'breadthfirst',
+
+    fit: true, // whether to fit the viewport to the graph
+    directed: false, // whether the tree is directed downwards (or edges can point in any direction if false)
+    padding: 30, // padding on fit
+    circle: false, // put depths in concentric circles if true, put depths top down if false
+    grid: false, // whether to create an even grid into which the DAG is placed (circle:false only)
+    spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
+    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+    nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
+    roots: undefined, // the roots of the trees
+    maximal: false, // whether to shift nodes down their natural BFS depths in order to avoid upwards edges (DAGS only)
+    depthSort: undefined, // a sorting function to order nodes at equal depth. e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+    animate: false, // whether to transition the node positions
+    animationDuration: 500, // duration of animation in ms if enabled
+    animationEasing: undefined, // easing of animation if enabled,
+    animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+    ready: undefined, // callback on layoutready
+    stop: undefined, // callback on layoutstop
+    transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
+};
+
+// var layout = cy.layout( options );
+
+
+
+var cy = cytoscape({
+    container: document.getElementById("cy"),
+
+    elements: [ // list of graph elements to start with
+        {data: { id: 10}},
+        {data: { id: 11}},
+        {data: { id: 'a' , parent: 10}},
+        {data: { id: 'b' , parent: 11}},
+        {data: { id: 'b' }},
+        {data: { id: 'b' }},
+        {data: { id: 'b' }},
+
+        {data: { id: 'ab', source: 10, target: 'b' }}
+    ],
+
+    style: [ // the stylesheet for the graph
+        {
+            selector: 'node',
+            style: {
+                'background-color': '#666',
+                'label': 'data(id)'
+            }
+        },
+
+        {
+            selector: 'edge',
+            style: {
+                'width': 3,
+                'line-color': '#ccc',
+                'target-arrow-color': '#ccc',
+                'target-arrow-shape': 'triangle',
+                'curve-style': 'bezier'
+            }
+        }
+    ],
+});
+
+cy.layout(options);
+
