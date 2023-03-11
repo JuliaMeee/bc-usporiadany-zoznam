84baa13ae0947734af06f5c01ccd4faf6e10d0b8
Julia Michalkova <xmichalk@fi.muni.cz>
2023-03-11 18:42:23 +0100

    js vis exploring options

--- js-vis/cytoscape/script.js
+++ js-vis/cytoscape/script.js
@@ -2,7 +2,7 @@ let options = {
     name: 'breadthfirst',
 
     fit: true, // whether to fit the viewport to the graph
-    directed: false, // whether the tree is directed downwards (or edges can point in any direction if false)
+    directed: true, // whether the tree is directed downwards (or edges can point in any direction if false)
     padding: 30, // padding on fit
     circle: false, // put depths in concentric circles if true, put depths top down if false
     grid: false, // whether to create an even grid into which the DAG is placed (circle:false only)
@@ -30,15 +30,18 @@ var cy = cytoscape({
     container: document.getElementById("cy"),
 
     elements: [ // list of graph elements to start with
-        {data: { id: 10}},
+/*        {data: { id: 10}},
         {data: { id: 11}},
         {data: { id: 'a' , parent: 10}},
         {data: { id: 'b' , parent: 11}},
-        {data: { id: 'b' }},
-        {data: { id: 'b' }},
-        {data: { id: 'b' }},
+        {data: { id: 'c' }},
+        {data: { id: 'd' }},
+        {data: { id: 'e' }},
 
-        {data: { id: 'ab', source: 10, target: 'b' }}
+        {data: { id: 'ab', source: 10, target: 'b' }},
+        {data: { id: 'ac', source: 10, target: 'c' }},
+        {data: { id: 'ad', source: 10, target: 'd' }},
+        {data: { id: 'ae', source: 10, target: 'e' }},*/
     ],
 
     style: [ // the stylesheet for the graph
@@ -63,5 +66,61 @@ var cy = cytoscape({
     ],
 });
 
-cy.layout(options);
+
+
+/*function addNode(cy, tag, val)
+{
+    let parentTag = tag.slice(0, -1)
+    console.log("parent tag:", parentTag)
+    if (cy.elements['node#parentTag'].length <= 0)
+    {
+        console.log("parent not found")
+        cy.add( { group: 'nodes', data: { id: parentTag} } )
+    }
+    cy.add( { group: 'nodes', data: { id: tag} } )
+    cy.add( { group: 'edges', data: { id: tag + "up", source: parentTag, target: tag } } )
+    if (val != null)
+    {
+        cy.add( { group: 'nodes', data: { id: val, parent: tag } } )
+    }
+}*/
+
+let level = 5
+let root = addTreeNode(cy, level, padLeft("", level, "_"));
+
+// cy.layout(options);
+
+options.roots = [root.data.id];
+
+var layout = cy.elements().layout(options);
+layout.run();
+
+cy.elements("node[id != 'mama']").forEach(console.log)
+
+function addTreeNode(diagram, level, tag) {
+    let node = { group: 'nodes', data: { id: tag } };
+    diagram.add( node );
+
+    if (level > 0) {
+        for (let i = 0; i < 2; i++)
+        {
+            let childTag = replaceAt(tag,tag.length - level - 1, i.toString());
+            let child = addTreeNode(diagram, level - 1, childTag);
+            diagram.add( { group: 'edges', data: { id: tag + "-" + childTag , source: tag, target: childTag } } );
+        }
+    }
+    else {
+        // diagram.add( { group: 'nodes', data: { id: tag + "\nval", parent: tag} })
+    }
+
+    return node;
+}
+
+function padLeft(str, length, char) {
+    return char.repeat(length - str.length) + str;
+}
+
+function replaceAt(str, index, char) {
+    return str.substring(0, index) + char + str.substring(index + char.length);
+}
 
